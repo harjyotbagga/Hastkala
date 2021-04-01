ba3f7c0b43597859c794b75cc9d9e7a3548a022f
Harjyot <imharjyotbagga@gmail.com>
2021-04-01 20:28:44 +0530

    user model schema

---
+++ src/models/user.js
@@ -0,0 +1,111 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+JSON_SECRET_TOKEN = process.env.JSON_SECRET_TOKEN;
+
+const userSchema = mongoose.Schema({
+    first_name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    last_name: {
+        type: String,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        valudate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Invalid Email Address");
+            }
+        },
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    phone: {
+        type: Number,
+        length: 10,
+    },
+    shipping: [{
+        'shipping_info': {
+            'address': {
+                type: String,
+                required: true
+            },
+            'city': {
+                type: String
+            },
+            'state': {
+                type: String
+            },
+            'country': {
+                type: String
+            },
+            'pin': {
+                type: Number,
+                length: 6
+            }
+        }
+    }],
+    tokens: [{
+        'token': {
+            type: String,
+            unique: true,
+            required: true
+        }
+    }]
+});
+
+userSchema.methods.generateAuthToken = async function() {
+    const user = this;
+    const token = jwt.sign({_id: user._id.String()}, JSON_SECRET_TOKEN);
+    user.tokens = user.tokens.concat({token});
+    await user.save();
+    return token;
+}
+
+userSchema.methods.toJSON = function () {
+    const user = this;
+    const userObject = user.toObject();
+    delete userObject.password;
+    delete userObject.tokens;
+    return userObject;
+}
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email }); 
+    if (!user) {
+        throw new Error('Unable to login.');
+    }
+    const isMatch = (user.password === password);
+    if (!isMatch) {
+        throw new Error('Unable to login.');
+    }
+    return user;
+}
+
+userSchema.pre('save', async function (next) {
+    const user = this;
+    if (!user.isModified('password')) return next();
+    user.password = await bcrypt.hash(user.password, 8);
+    next();
+});
+
+userSchema.pre('remove', async function (next) {
+    const user = this;
+    // Cascading
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = new express.Router();
+const User = require('../models/user');
 
 router.get('/login', (req, res) => {
     res.render('login');
@@ -7,6 +8,26 @@ router.get('/login', (req, res) => {
 
 router.get('/register', (req, res) => {
     res.render('register');
+});
+
+router.post('/login', async (req, res) => {
+    const user = User(req.body);
+    try {
+        console.log(user);
+        res.status(201).send();
+    } catch (e) {
+        res.send(e);
+    }
+});
+
+router.post('/register', async (req, res) => {
+    const user = User(req.body);
+    try {
+        console.log(user);
+        res.status(201).send();
+    } catch (e) {
+        res.send(e);
+    }
 });
 
 module.exports = router;
